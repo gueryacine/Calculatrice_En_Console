04162ba5f7e27bb39b709095a4169a3ed3be0003
gueryacine <gueryacine@hotmail.com>
2021-05-27 19:10:52 +0300

    Update main.c

--- Calculatrice_Graphique_En_Console/main.c
+++ Calculatrice_Graphique_En_Console/main.c
@@ -3,6 +3,7 @@
 
 int main()
 {
-    printf("Hello world! integration partie1\n");
+    printf("Hello world! integration partie1 \n");
+    printf("Hello world! fonctionalite \n");
     return 0;
 }
